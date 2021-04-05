178942d8e4b772d620ff0d2dade5773dedf23112
henrique <henriquecesps4@gmail.com>
2021-04-05 02:34:28 -0300

    feat(app): empty state & loading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,14 @@ import Layout from "./components/layout";
 import { useLazyQuery } from "@apollo/client";
 import { GET_QUESTION } from "./components/graphql";
 import QuestionCard, { Question } from "./components/cards/QuestionCard";
+import FormSearch from "./components/form/FormSearch";
+import EmptyState from "./components/cards/EmptyState";
+import { ReactComponent as Loading } from "./components/assets/loading.svg";
 
 function App(): JSX.Element {
-  const [getQuestions, { data, called }] = useLazyQuery(GET_QUESTION, {
+  const [getQuestions, { data, called, loading }] = useLazyQuery(GET_QUESTION, {
     variables: {
-      tags: "javascript",
+      tags: `javascript;`,
     },
   });
 
@@ -18,10 +21,15 @@ function App(): JSX.Element {
   return (
     <Layout>
       {!called && <button onClick={() => getQuestions()}>BUSCAR</button>}
-      {data &&
+      <FormSearch />
+      {loading && <Loading />}
+      {data ? (
         data.getQuestions.map((question: Question) => (
           <QuestionCard question={question} key={question.question_id} />
-        ))}
+        ))
+      ) : (
+        <EmptyState called={called} loading={loading} />
+      )}
     </Layout>
   );
 }
