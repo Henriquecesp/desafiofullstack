b5b36fd2b1b3fb2bb87933da40f2c86a3c4a68ea
henrique <henriquecesps4@gmail.com>
2021-04-05 02:52:27 -0300

    feat(card): question score on card

--- frontend/src/components/cards/QuestionCard.tsx
+++ frontend/src/components/cards/QuestionCard.tsx
@@ -99,7 +99,7 @@ const QuestionCard = ({ question }: IQuestion): JSX.Element => {
             </svg>
             {nFormatter(Number(question.view_count))}
           </span>
-          <span className="text-gray-500 inline-flex items-center leading-none text-sm">
+          <span className="text-gray-500 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-800">
             <svg
               className="w-4 h-4 mr-1"
               stroke="currentColor"
@@ -111,7 +111,21 @@ const QuestionCard = ({ question }: IQuestion): JSX.Element => {
             >
               <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
             </svg>
-            {nFormatter(Number(question.answer_count))}
+            {nFormatter(Number(question.view_count))}
+          </span>
+          <span className="text-gray-500 inline-flex items-center leading-none text-sm">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2.5"
+              className="w-4 h-4 mr-1"
+              viewBox="0 0 24 24"
+            >
+              <path d="M20 6L9 17l-5-5"></path>
+            </svg>
+            {nFormatter(Number(question.score))}
           </span>
         </div>
       </div>
