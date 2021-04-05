3f00e1f083fa4399f7d231f3df6508db5d2a3405
henrique <henriquecesps4@gmail.com>
2021-04-05 00:30:39 -0300

    feat(resolver): api send params to request

--- backend/src/resolver.js
+++ backend/src/resolver.js
@@ -2,16 +2,29 @@ const axios = require("axios");
 
 exports.resolvers = {
   Query: {
-    getQuestions() {
+    getQuestions(_, params) {
       const api = "https://api.stackexchange.com/2.2";
       const method = "questions";
+      const site = "&site=stackoverflow";
+      const order = "&order=desc";
+      const filter = "&filter=default";
+      const score = `&min=${params.score || 0}`;
+      const sort = `&sort=${params.sort || "votes"}`;
+      const limit = `&pagesize=${params.limit || 6}`;
+      const tags = `&tagged=${params.tags}`;
 
-      return axios.get(`${api}/${method}`).then((res) => {
-        console.log(data.items);
-        return res.data.items.map((question) => {
-          return question;
+      return axios
+        .get(
+          `${api}/${method}?${site}${order}${score}${sort}${limit}${tags}${filter}`
+        )
+        .then((res) => {
+          return res.data.items.map((question) => {
+            return question;
+          });
+        })
+        .catch((err) => {
+          return err.response.data;
         });
-      });
     },
   },
 };
