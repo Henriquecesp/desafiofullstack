0558a439c4ad38ebe0186fade238c99783329e20
henrique <henriquecesps4@gmail.com>
2021-04-05 01:44:49 -0300

    feat(app): query and pass to component card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import Layout from "./components/layout";
+import { useLazyQuery } from "@apollo/client";
+import { GET_QUESTION } from "./components/graphql";
+import QuestionCard, { Question } from "./components/cards/QuestionCard";
 
 function App(): JSX.Element {
+  const [getQuestions, { data, called }] = useLazyQuery(GET_QUESTION, {
+    variables: {
+      tags: "javascript",
+    },
+  });
+
+  if (data) {
+    console.log(data);
+  }
+
   return (
     <Layout>
-      <h1>Hello Front-End</h1>
+      {!called && <button onClick={() => getQuestions()}>BUSCAR</button>}
+      {data &&
+        data.getQuestions.map((question: Question) => (
+          <QuestionCard question={question} key={question.question_id} />
+        ))}
     </Layout>
   );
 }
